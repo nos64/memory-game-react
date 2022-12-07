ffaf22167959920ab11c4da48add45827050b2e1
nos64 <manosov1984@gmail.com>
2022-12-07 15:06:38 +0400

    feat: add card Data

---
+++ src/common/cardData.ts
@@ -0,0 +1,165 @@
+import reverse from '../assets/images/avengers-logo.png';
+import antMan from '../assets/images/cards/ant-man_5-5.jpg';
+import blackPanter1 from '../assets/images/cards/black-panter_5-5.jpg';
+import blackPanter2 from '../assets/images/cards/black-panter1_5-5.jpg';
+import cptAmerica1 from '../assets/images/cards/cpt-america_5-5.jpg';
+import cptAmerica2 from '../assets/images/cards/cpt-america1_5-5.jpg';
+import blackPAnter from '../assets/images/cards/black-panter_5-5.jpg';
+import cptMarvel from '../assets/images/cards/cpt-marvel_5-5.jpg';
+import drStrange from '../assets/images/cards/dr-starange_5-5.jpg';
+import eagle from '../assets/images/cards/eagle_5-5.jpg';
+import fury from '../assets/images/cards/fury_5-5.jpg';
+import hawkEye1 from '../assets/images/cards/hawk-eye_5-5.jpg';
+import hawkEye2 from '../assets/images/cards/hawk-eye1_5-5.jpg';
+import hulk1 from '../assets/images/cards/hulk_5-5.jpg';
+import hulk2 from '../assets/images/cards/hulk1_5-5.jpg';
+import hulk3 from '../assets/images/cards/hulk2_5-5.jpg';
+import ironMan1 from '../assets/images/cards/iron-man_5-5.jpg';
+import ironMan2 from '../assets/images/cards/iron-man1_5-5.jpg';
+import locky from '../assets/images/cards/locky_5-5.jpg';
+import romanov from '../assets/images/cards/romanov_5-5.jpg';
+import spiderMan1 from '../assets/images/cards/spider-man_5-5.jpg';
+import spiderMan2 from '../assets/images/cards/spider-man1_5-5.jpg';
+import thor1 from '../assets/images/cards/thor_5-5.jpg';
+import thor2 from '../assets/images/cards/thor1_5-5.jpg';
+
+export const cardData = [
+  {
+    id: '01',
+    avers: antMan,
+    reverse: reverse,
+    isActive: false,
+  },
+  {
+    id: '02',
+    avers: blackPanter1,
+    reverse: reverse,
+    isActive: false,
+  },
+  {
+    id: '03',
+    avers: blackPanter2,
+    reverse: reverse,
+    isActive: false,
+  },
+  {
+    id: '04',
+    avers: cptAmerica1,
+    reverse: reverse,
+    isActive: false,
+  },
+  {
+    id: '05',
+    avers: cptAmerica2,
+    reverse: reverse,
+    isActive: false,
+  },
+  {
+    id: '06',
+    avers: blackPAnter,
+    reverse: reverse,
+    isActive: false,
+  },
+  {
+    id: '07',
+    avers: cptMarvel,
+    reverse: reverse,
+    isActive: false,
+  },
+  {
+    id: '08',
+    avers: drStrange,
+    reverse: reverse,
+    isActive: false,
+  },
+  {
+    id: '09',
+    avers: eagle,
+    reverse: reverse,
+    isActive: false,
+  },
+  {
+    id: '10',
+    avers: fury,
+    reverse: reverse,
+    isActive: false,
+  },
+  {
+    id: '11',
+    avers: hawkEye1,
+    reverse: reverse,
+    isActive: false,
+  },
+  {
+    id: '12',
+    avers: hawkEye2,
+    reverse: reverse,
+    isActive: false,
+  },
+  {
+    id: '13',
+    avers: hulk1,
+    reverse: reverse,
+    isActive: false,
+  },
+  {
+    id: '14',
+    avers: hulk2,
+    reverse: reverse,
+    isActive: false,
+  },
+  {
+    id: '15',
+    avers: hulk3,
+    reverse: reverse,
+    isActive: false,
+  },
+  {
+    id: '16',
+    avers: ironMan1,
+    reverse: reverse,
+    isActive: false,
+  },
+  {
+    id: '17',
+    avers: ironMan2,
+    reverse: reverse,
+    isActive: false,
+  },
+  {
+    id: '18',
+    avers: locky,
+    reverse: reverse,
+    isActive: false,
+  },
+  {
+    id: '19',
+    avers: romanov,
+    reverse: reverse,
+    isActive: false,
+  },
+  {
+    id: '20',
+    avers: spiderMan1,
+    reverse: reverse,
+    isActive: false,
+  },
+  {
+    id: '21',
+    avers: spiderMan2,
+    reverse: reverse,
+    isActive: false,
+  },
+  {
+    id: '22',
+    avers: thor1,
+    reverse: reverse,
+    isActive: false,
+  },
+  {
+    id: '22',
+    avers: thor2,
+    reverse: reverse,
+    isActive: false,
+  },
+];
