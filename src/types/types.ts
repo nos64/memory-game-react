8c08ea84921ff9745d7da6dc2c79e70534a2df0e
nos64 <manosov1984@gmail.com>
2022-12-09 14:58:36 +0400

    fix: delete comments

--- src/types/types.ts
+++ src/types/types.ts
@@ -2,13 +2,9 @@ export interface IStartForm {
   playerName: string;
   difficulty: string;
   cardList: ICard[];
-<<<<<<< HEAD
   isGameStart: boolean;
   pausedTimer: boolean;
 }
-=======
-};
->>>>>>> 3f2647ea46fb5e02758f667647067e7721666c54
 
 export interface ICard {
   id: string;
