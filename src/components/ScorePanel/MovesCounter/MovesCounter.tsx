7c3b772c552fdafd836afd505fc6ac2d8997491f
Mikhail Nosov <manosov1984@gmail.com>
2022-12-17 00:51:35 +0400

    fix: fix panel

--- src/components/ScorePanel/MovesCounter/MovesCounter.tsx
+++ src/components/ScorePanel/MovesCounter/MovesCounter.tsx
@@ -5,7 +5,7 @@ import styles from './MovesCounter.module.scss';
 const MovesCounter = () => {
   const moves = useAppSelector((state) => state.game.moves);
   return (
-    <div className={styles.paramWrapper}>
+    <div className={styles.fieldWrapper}>
       Moves:
       <div className={styles.paramField}>{moves}</div>
     </div>
